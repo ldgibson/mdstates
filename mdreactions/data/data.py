580624369b92af8f55b6604946a26e6dea045abd
Luke Gibson <ldgibson@uw.edu>
2018-03-08 22:22:21 -0800

    added functions for loading bond distance csv file

---
+++ mdreactions/data/data.py
@@ -0,0 +1,4 @@
+from .util import getpath, loadfile
+
+
+bonds = loadfile(getpath('bond_distances.csv'))
---
+++ mdreactions/util.py
@@ -0,0 +1,18 @@
+from os.path import abspath, dirname, join
+
+import pandas as pd
+
+
+def getpath(filename):
+    """
+    Returns the absolute path of a file in the data directory.
+    """
+    return abspath(join(dirname(__file__), "data", filename))
+
+
+def loadfile(file_path, **kwargs):
+    """
+    Loads the file at the given path into a pandas dataframe
+    and returns it.
+    """
+    return pd.read_csv(file_path, **kwargs)

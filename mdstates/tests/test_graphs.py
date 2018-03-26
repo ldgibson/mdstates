9925386adbc71d3aac02d1a89a0b8030397ade9c
Luke Gibson <ldgibson@uw.edu>
2018-03-26 15:31:56 -0700

    added unit tests for `graphs` module

---
+++ mdstates/tests/test_graphs.py
@@ -0,0 +1,54 @@
+from ..graphs import *
+
+
+def test_combine_graphs():
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3])
+    G.node[2]['color'] = 'blue'
+    G.add_edges_from([(1, 2, {'weight': 1}), (2, 3, {'weight': 1})])
+    
+    H = nx.Graph()
+    H.add_nodes_from([2, 3, 4])
+    H.node[3]['color'] = 'red'
+    H.add_edges_from([(2, 3, {'weight': 1}), (3, 4, {'weight': 1})])
+    
+    combined = combine_graphs(G, H)
+    
+    assert list(combined.nodes) == [1, 2, 3, 4],\
+        "Node lists are not the same."
+    
+    assert combined.node[2]['color'] == 'blue',\
+        "Not propagating node attributes."
+    assert combined.node[3]['color'] == 'red',\
+        "Not propagating node attributes."
+
+    assert combined[2][3]['weight'] == 2,\
+        "Not properly summing edge attributes."
+
+    true_edges = [(1, 2, {'weight': 1}),
+                  (2, 3, {'weight': 2}),
+                  (3, 4, {'weight': 1})]
+    
+    for edge in combined.edges(data=True):
+        assert edge in true_edges, "Incorrect edges."
+    return
+
+def test_combined_graph_edges():
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3])
+    G.add_edges_from([(1, 2, {'weight': 1}), (2, 3, {'weight': 1})])
+    
+    H = nx.Graph()
+    H.add_nodes_from([2, 3, 4])
+    H.add_edges_from([(2, 3, {'weight': 1}), (3, 4, {'weight': 1})])
+    
+    true_edges = [(1, 2, {'weight': 1}),
+                  (2, 3, {'weight': 2}),
+                  (3, 4, {'weight': 1})]
+    
+    for u, v, data in combined_graph_edges(G, H):
+        assert (u, v) in set(G.edges) | set(H.edges),\
+            "Edge pair not found in original graphs."
+        assert (u, v, data) in true_edges,\
+            "Not all edges are found."
+    return

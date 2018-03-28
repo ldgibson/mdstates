af14ca2df90ac0ec8dded8b3a2924e515f0fe47e
Luke Gibson <ldgibson@uw.edu>
2018-03-27 17:58:05 -0700

    added function to merge nodes, updated merge edges, working better

--- mdstates/graphs.py
+++ mdstates/graphs.py
@@ -1,8 +1,36 @@
 from itertools import chain
+from numbers import Number
 
 import networkx as nx
+import numpy as np
 
-__all__ = ['combined_graph_edges', 'combine_graphs']
+__all__ = ['combined_graph_nodes', 'combined_graph_edges', 'combine_graphs']
+
+
+def combined_graph_nodes(G, H):
+    intersect = set(G.nodes) & set(H.nodes)
+    disj = set(G.nodes) ^ set(H.nodes)
+
+    for n in chain(intersect, disj):
+        if n in G.nodes and n in H.nodes:
+            attr = G.node[n].copy()
+            hdata = H.node[n]
+
+            # All node attributes that are shared.
+            shared = set(attr) & set(hdata)
+            for key in shared:
+                if isinstance(attr[key], Number):
+                    attr.update({key: attr[key] + hdata[key]})
+
+            # All node attributes that are not shared.
+            not_shared = set(hdata) - set(attr)
+            attr.update((key, hdata[key]) for key in not_shared)
+        elif n in G.nodes:
+            attr = G.node[n]
+        else:
+            attr = H.node[n]
+
+        yield n, attr
 
 
 def combined_graph_edges(G, H):
@@ -13,7 +41,13 @@ def combined_graph_edges(G, H):
         if (u, v) in G.edges and (u, v) in H.edges:
             attr = G[u][v].copy()
             hdata = H[u][v]
-            attr.update((key, attr[key] + hdata[key]) for key in attr.keys())
+            shared = set(attr) & set(hdata)
+            for key in shared:
+                if isinstance(attr[key], Number):
+                    attr.update({key: attr[key] + hdata[key]})
+
+            not_shared = set(hdata) - set(attr)
+            attr.update((key, hdata[key]) for key in not_shared)
         elif (u, v) in G.edges:
             attr = G[u][v]
         else:
@@ -24,8 +58,9 @@ def combined_graph_edges(G, H):
 
 def combine_graphs(G, H):
     graph = nx.DiGraph()
-    graph.add_nodes_from(G.nodes(data=True))
-    graph.add_nodes_from(node for node in H.nodes(data=True)
-                         if node not in G.nodes(data=True))
+    graph.add_nodes_from(combined_graph_nodes(G, H))
+    # graph.add_nodes_from(G.nodes(data=True))
+    # graph.add_nodes_from(node for node in H.nodes(data=True)
+    #                      if node not in G.nodes(data=True))
     graph.add_edges_from(combined_graph_edges(G, H))
     return graph

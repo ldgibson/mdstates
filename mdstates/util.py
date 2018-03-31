c9d50a56940d57f2b5ca246947a6754594a9eb84
Luke Gibson <ldgibson@uw.edu>
2018-03-30 17:54:32 -0700

    added Scaler class

--- mdstates/util.py
+++ mdstates/util.py
@@ -1,5 +1,7 @@
 from os.path import abspath, dirname, join
+from numbers import Number
 
+import numpy as np
 import pandas as pd
 
 
@@ -16,3 +18,34 @@ def loadfile(filename, **kwargs):
     and returns it.
     """
     return pd.read_csv(getpath(filename), **kwargs)
+
+
+class Scaler:
+    def __init__(self, target_min=0, target_max=1):
+        self.target_min = target_min
+        self.target_max = target_max
+        self.min_val = None
+        self.max_val = None
+        return
+
+    def set_data_range(self, min_val, max_val):
+        assert min_val <= max_val,\
+            "First argument must be less or equal to than second argument."
+        self.max_val = max_val
+        self.min_val = min_val
+        return
+
+    def transform(self, data):
+        if isinstance(data, Number):
+            if data > self.max_val or data < self.min_val:
+                raise ValueError("{} is out of range.".format(data))
+            else:
+                return (data - self.min_val) / (self.max_val - self.min_val) *\
+                    (self.target_max - self.target_min) + self.target_min
+        elif isinstance(data, list) or isinstance(data, np.ndarray):
+            for d in data:
+                if d > self.max_val or data < self.min_val:
+                    raise ValueError("{} is out of range.".format(d))
+            return [(d - self.min_val) / (self.max_val - self.min_val) *
+                    (self.target_max - self.target_min) + self.target_min
+                    for d in data]

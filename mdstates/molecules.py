3d9943685b76394273771c493fe4ca4db48db751
Luke Gibson <ldgibson@uw.edu>
2018-04-06 11:10:29 -0700

    added lines to force ions to not be bonded. also removed `Molecule` class that was being built.

--- mdstates/molecules.py
+++ mdstates/molecules.py
@@ -56,6 +56,12 @@ def set_structure(mol, cmat, atom_list):
         mol.AddAtom(Chem.Atom(atom))
 
     for i, j in zip(*np.where(cmat[:, :] == 1)):
+        if mol.GetAtomWithIdx(int(i)).GetSymbol() == 'Li' or\
+                mol.GetAtomWithIdx(int(j)).GetSymbol() == 'Li':
+            continue
+        else:
+            pass
+
         mol.AddBond(int(i), int(j), Chem.BondType.SINGLE)
 
     for at in mol.GetAtoms():
@@ -131,19 +137,3 @@ def set_positive_charges(graph):
         else:
             pass
     return
-
-
-class Molecule:
-    def __init__(self):
-        self._atoms = []
-        self._bonds = []
-        return
-
-    def add_atom(self, atom_sym, charge=0, local_multiplicity=1):
-        self._atoms.append({'symbol': atom_sym, 'charge': charge,
-                            'local_multiplicity': local_multiplicity})
-        return
-
-    @property
-    def atoms(self):
-        return [atom['symbol'] for atom in self._atoms]

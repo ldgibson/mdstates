c0e90e521aaeb97ef09bd049cc704d74ac7737b0
Luke Gibson <ldgibson@uw.edu>
2018-04-07 11:24:35 -0700

    removed old `generate_SMILES` fmethod and updated smiles imports

--- mdstates/main.py
+++ mdstates/main.py
@@ -13,8 +13,7 @@ from .data import bonds
 from .graphs import combine_graphs, _prepare_graph
 from .hmm import generate_ignore_list, viterbi
 from .molecules import contact_matrix_to_SMILES
-from .smiles import reduceSMILES, save_unique_SMILES, _break_ionic_bonds,\
-    _radical_to_sp2
+from .smiles import save_unique_SMILES
 
 __all__ = ['Network']
 
@@ -489,59 +488,6 @@ class Network:
 #                else:
 #                    self.frames[rep_id].append(f)
         return
-
-    def generate_SMILES_old(self, rep_id, tol=2, split_ions=True):
-        """Generates list of SMILES strings from trajectory.
-
-        Parameters
-        ----------
-        rep_id : int
-            Replica identifier.
-        tol : int, optional
-            Trajectory frames that are +/- `tol` frames from transition
-            frames will be converted to SMILES strings. Default is 2.
-
-        Returns
-        -------
-        smiles : list of str
-            List of SMILES strings compiled from all trajectory frames
-            within the specified tolerance of transition frames.
-        """
-
-        warnings.warn("The only acceptable file format is XYZ from CP2K.",
-                      RuntimeWarning)
-
-        frames = self.frames[rep_id].copy()
-        frames.insert(0, 0)
-
-        smiles = []
-
-        for mol in pybel.readfile("xyz", self.replica[rep_id]['path']):
-            n_frame = int(re.findall("\d+", mol.title.split(',')[0])[0]) // 100
-            if np.isclose(n_frame, frames, atol=2).any():
-                # Read in frame and convert to SMILES.
-                pybelsmiles = mol.write("smiles").split('\t')[0]
-
-                # Break any Li-O ionic bonds.
-                pybelsmiles = _break_ionic_bonds(pybelsmiles)
-
-                # Change how SMILES is written using RDKit.
-                mol = Chem.MolFromSmiles(pybelsmiles)
-                new_smiles = Chem.MolToSmiles(mol)
-
-                # Change any ternary radial carbons into sp2
-                # hybridization to stay consistent.
-                new_smiles = _radical_to_sp2(new_smiles)
-
-                # Change SMILES back into RDKit format.
-                mol = Chem.MolFromSmiles(new_smiles)
-                smiles.append(Chem.MolToSmiles(mol))
-
-            else:
-                pass
-
-        smiles = reduceSMILES(smiles)
-        return smiles
 
     def generate_SMILES(self, rep_id, tol=2,
                         first_smiles='O=C1OCCO1.O=C1OCCO1.[Li]'):

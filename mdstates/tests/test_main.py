0fe680f96373ed0bb890ad26f622d7237dbe4968
Luke Gibson <ldgibson@uw.edu>
2018-04-13 16:18:14 -0700

    added unit tests for `_generate_pairs`, `set_cutoff`, and `decode` methods

--- mdstates/tests/test_main.py
+++ mdstates/tests/test_main.py
@@ -72,3 +72,84 @@ def test_generate_contact_matrix():
         (n_frames, net.n_atoms, net.n_atoms)
 
     return
+
+
+def test__generate_pairs():
+    net = Network()
+    net.add_replica(traj_path, top_path)
+
+    net._generate_pairs()
+
+    pairs = np.triu_indices(6, 1)
+
+    for i, j in zip(*pairs):
+        assert [i, j] in net._pairs
+
+    return
+
+
+def test_set_cutoff():
+    net = Network()
+    net.add_replica(traj_path, top_path)
+
+    try:
+        net.set_cutoff('CC', 0.2)
+    except(TypeError):
+        pass
+    else:
+        raise Exception("Non-list allowed.")
+
+    try:
+        net.set_cutoff(['C'], 0.2)
+    except(ValueError):
+        pass
+    else:
+        raise Exception("Single element allowed.")
+
+    try:
+        net.set_cutoff(['C', 'C', 'C'], 0.2)
+    except(ValueError):
+        pass
+    else:
+        raise Exception("Three atoms allowed.")
+
+    try:
+        net.set_cutoff(['C', 'C'], 0)
+    except(ValueError):
+        pass
+    else:
+        raise Exception("Zero cutoff allowed.")
+
+    try:
+        net.set_cutoff(['C', 'C'], -0.2)
+    except(ValueError):
+        pass
+    else:
+        raise Exception("Negative cutoff allowed.")
+
+    net.set_cutoff(['Li', 'N'], 0.3)
+
+    assert net._cutoff[frozenset(['Li', 'N'])] == 0.3
+
+    return
+
+
+def test_decode():
+    net = Network()
+    net.replica.append({'traj': None, 'cmat': None, 'path': None,
+                        'processed': False, 'network': None})
+    net.n_atoms = 3
+    net.frames.append([])
+
+    net.replica[0]['cmat'] = np.zeros((200, 3, 3), dtype=int)
+    net.replica[0]['cmat'][:, 0, 1] = 1
+    idx = np.arange(0, 200, 10)
+    net.replica[0]['cmat'][idx, 0, 2] = 1
+
+    net.decode()
+
+    assert np.all(net.replica[0]['cmat'][:, 0, 1] == 1)
+    assert np.all(net.replica[0]['cmat'][:, 1, 2] == 0)
+    assert np.all(net.replica[0]['cmat'][:, 0, 2] == 0)
+
+    return

8bd72204d45922a6ece760aae4bea80dbe9a50b3
Luke Gibson <ldgibson@uw.edu>
2018-09-26 13:55:44 -0700

    converted the "V" and "prev" arrays to std::vectors and initilized them to 0s. this prevents segmentation faults.

--- mdstates/hmm.cpp
+++ mdstates/hmm.cpp
@@ -8,32 +8,18 @@ std::vector<int> decoder(std::vector<int> obs, const int num_frames,
                          std::vector<float> start_p,
                          std::vector<std::vector<float>> trans_p,
                          std::vector<std::vector<float>> emission_p) {
-    // float start_p[2] = { 0.5, 0.5 };
-    // float trans_p[2][2] = { { 0.999, 0.001 },
-                            // { 0.001, 0.999 } };
-    // float emission_p[2][2] = { { 0.60, 0.40 },
-                               // { 0.40, 0.60 } };
 
-    // float start_p[2] = {start_p_val, float(1.0 - start_p_val)};
-    // float trans_p[2][2];
-    // trans_p[0][0] = trans_p_val;
-    // trans_p[1][1] = trans_p_val;
-    // trans_p[0][1] = 1.0 - trans_p_val;
-    // trans_p[1][0] = 1.0 - trans_p_val;
-
-    // float emission_p[2][2];
-    // emission_p[0][0] = emission_p_val;
-    // emission_p[1][1] = emission_p_val;
-    // emission_p[0][1] = 1.0 - emission_p_val;
-    // emission_p[1][0] = 1.0 - emission_p_val;
-
-    float(*V)[2] = new float[num_frames][2];
-    int(*prev)[2] = new int[num_frames][2];
+    // float(*V)[2] = new float[num_frames][2];
+    // int(*prev)[2] = new int[num_frames][2];
+    std::vector < std::vector <float> > V (num_frames, std::vector <float> (2, 0));
+    std::vector < std::vector <int> > prev (num_frames, std::vector <int> (2, 0));
 
     for (int st = 0; st < 2; st++) {
             V[0][st] = log10(start_p[st] * emission_p[st][obs[0]]);
     }
 
+    // printf("num_frames = %d\n", num_frames);
+    // printf("before loop\n");
     float max_tr_prob[2];
     float max_tr, max_prob;
     for (int t = 1; t < num_frames; t++) {
@@ -52,22 +38,24 @@ std::vector<int> decoder(std::vector<int> obs, const int num_frames,
             }
         }
     }
-    std::vector <int> optimal_path;
+    // printf("After loop\n");
+    std::vector <int> optimal_path (num_frames, 0);
     float max_final_prob = std::max(V[num_frames - 1][0], V[num_frames - 1][1]);
     int previous;
 
     for (int st = 0; st < 2; st++) {
         if (V[num_frames - 1][st] == max_final_prob) {
-            optimal_path.push_back(st);
+            optimal_path[num_frames - 1] = st;
             previous = st;
         }
     }
 
     for (int t = num_frames - 2; t >= 0; t--) {
-        optimal_path.insert(optimal_path.begin(), prev[t + 1][previous]);
+        optimal_path[t] = prev[t + 1][previous];
         previous = prev[t + 1][previous];
     }
-    delete [] V;
-    delete [] prev;
+    // printf("after decoding step\n");
+    // delete [] V;
+    // delete [] prev;
     return optimal_path;
 }
